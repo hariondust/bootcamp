094c863df241652d63fc88d8b4fa3dc67f2fa820
hariondust <57011893+hariondust@users.noreply.github.com>
2019-10-26 20:10:51 +0700

    Add files via upload

---
+++ Nomor7/header.php
@@ -0,0 +1,33 @@
+<!-- Load Model -->
+<?php require_once('m_biodata.php'); ?>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <title>Nurul Fikri - Bootcamp</title>
+</head>
+
+<body>
+
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-dark  bg-info">
+        <a class="navbar-brand mr-5 ml-3 text-uppercase" href="http://wwww.instagram.com/nfikrii">Nurul Fikri</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav text-uppercase">
+                <a class="nav-item nav-link" href="">Home </a>
+                <a class="nav-item nav-link" href="view-tambahbuku.php">Tambah Buku</a>
+                <a class="nav-item nav-link" href="http://www.instagram.com/nfikrii" target="_blank">Follow</a>
+            </div>
+        </div>
+    </nav>
